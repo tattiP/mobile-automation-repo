f51a8e59ab78c0ec7fe65606b2e4e6fe8fdc93f8
tattiP <potatiana5@gmail.com>
2023-07-18 16:18:55 -0400

    Class Created
    MainClass created
    getLocalNumber method created
    Test created

---
+++ MainClass.java
@@ -0,0 +1,11 @@
+public class MainClass {
+    public int getLocalNumber()
+    {
+        return 14;
+    }
+}
+
+
+
+
+
---
+++ MainClassTest.java
@@ -0,0 +1,19 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+public class MainClassTest extends MainClass
+{
+
+    @Test
+    public void testGetLocalNumber() {
+        System.out.println(getLocalNumber());
+    }
+    {
+        int expected = 14;
+        int actual = 2 * 7;
+
+        Assert.assertTrue("2 * 7 != 14",actual == expected);
+        }
+    }
+
+
