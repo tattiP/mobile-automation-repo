396528dfbf554d2a0aaa678ba919b5c24cc109a3
tattiP <potatiana5@gmail.com>
2023-07-19 12:34:52 -0400

    Update MainClassTest2.java

--- MainClassTest2.java
+++ MainClassTest2.java
@@ -6,11 +6,12 @@ public class MainClassTest2 extends MainClass2 {
     @Test
     public void testGetClassNumber() {
         int a = 20;
+        int b = 45;
 
-        if (a < 45) {
-        System.out.println("True");
+        if (a > 45) {
+        System.out.println("This will never happen");
     }   else {
-            System.out.println("False");
+            System.out.println("This is what will happen");
         }
         Assert.fail("This message is displayed");
 
