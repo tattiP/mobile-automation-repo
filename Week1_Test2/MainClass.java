cbe3ff085a0d4d2cd018c1dbaf71c47878febc69
tattiP <potatiana5@gmail.com>
2023-07-27 17:43:00 -0400

    Week1_test2_second attempt

---
+++ Week1_Test2/MainClass.java
@@ -0,0 +1,9 @@
+import org.junit.Test;
+
+public class MainClass {
+    private int class_number = 20;
+
+    public int getClassNumber() {
+        return 20;
+    }
+}
---
+++ Week1_Test2/MainClassTest.java
@@ -0,0 +1,14 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+public class MainClassTest extends MainClass {
+    @Test
+    public void testGetClassNumber(){
+        if (getClassNumber() > 45) {
+            System.out.println("True");
+        } else {
+            System.out.println("False");
+        }
+
+    }
+}
